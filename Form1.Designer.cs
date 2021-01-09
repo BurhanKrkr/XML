82b6f77b1dd29527b077e8f8fca1858a36991f53
BurhanKrkr <burhannkrkr@gmail.com>
2021-01-09 20:09:50 +0300

    XML DOSYA İŞLEMLERİ

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace XML_DOSYA_İŞLEMLERİ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnToXml = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnReadXML = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(22, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyisim :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(17, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "D. Tarihi :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(23, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Meslek :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(39, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "İsim :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(76, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 21);
+            this.textBox3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnToXml);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 175);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "XML Dosya Oluşturma";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(76, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(109, 21);
+            this.textBox4.TabIndex = 11;
+            // 
+            // btnToXml
+            // 
+            this.btnToXml.Location = new System.Drawing.Point(26, 146);
+            this.btnToXml.Name = "btnToXml";
+            this.btnToXml.Size = new System.Drawing.Size(159, 23);
+            this.btnToXml.TabIndex = 12;
+            this.btnToXml.Text = "XML Dosyası Oluştur";
+            this.btnToXml.UseVisualStyleBackColor = true;
+            this.btnToXml.Click += new System.EventHandler(this.btnToXml_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnReadXML);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(288, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(265, 175);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "XML Dosya Okuma";
+            // 
+            // btnReadXML
+            // 
+            this.btnReadXML.Location = new System.Drawing.Point(26, 146);
+            this.btnReadXML.Name = "btnReadXML";
+            this.btnReadXML.Size = new System.Drawing.Size(159, 23);
+            this.btnReadXML.TabIndex = 12;
+            this.btnReadXML.Text = "XML Dosyası Oku";
+            this.btnReadXML.UseVisualStyleBackColor = true;
+            this.btnReadXML.Click += new System.EventHandler(this.btnReadXML_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(76, 26);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(109, 21);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(76, 54);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(109, 21);
+            this.textBox6.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Soyisim :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(76, 80);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(109, 21);
+            this.textBox7.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(17, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "D. Tarihi :";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(76, 107);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(109, 21);
+            this.textBox8.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(23, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Meslek :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(39, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "İsim :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 194);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "XML";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnToXml;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnReadXML;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_DOSYA_İŞLEMLERİ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnToXml.Click += btnToXml_Click;
+            btnReadXML.Click += btnReadXML_Click;
+        }
+
+        private void btnToXml_Click(object sender, EventArgs e)
+        {
+            foreach(Control c in groupBox1.Controls)
+            {
+                if(c is TextBox)
+                {
+                    if(c.Text == "")
+                    {
+                        return;
+                    }
+                }
+            }
+            //XmlDocument xdoc = new XmlDocument();
+            //XmlNode root = xdoc.CreateElement("Kullanicilar");
+            //xdoc.AppendChild(root);
+
+            
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    XmlNode Kullanici = xdoc.CreateElement("Kullanici");
+            //    XmlAttribute ID = xdoc.CreateAttribute("ID");
+            //    XmlNode Ad = xdoc.CreateElement("Ad");
+            //    XmlNode Soyad = xdoc.CreateElement("Soyad");
+            //    XmlNode DTarih = xdoc.CreateElement("DTarih");
+            //    XmlNode Meslek = xdoc.CreateElement("Meslek");
+
+
+            //    ID.Value = i.ToString();
+            //    Ad.InnerText = textBox1.Text;
+            //    Soyad.InnerText = textBox2.Text;
+            //    DTarih.InnerText = textBox3.Text;
+            //    Meslek.InnerText = textBox4.Text;
+
+            //    Kullanici.Attributes.Append(ID);
+            //    Kullanici.AppendChild(Ad);
+            //    Kullanici.AppendChild(Soyad);
+            //    Kullanici.AppendChild(DTarih);
+            //    Kullanici.AppendChild(Meslek);
+            //    root.AppendChild(Kullanici);
+            //}
+
+
+            using(SaveFileDialog sf = new SaveFileDialog())
+            {
+                sf.Filter = "XML Dosyası|*.xml";
+                if(sf.ShowDialog() == DialogResult.OK)
+                {
+                    //xdoc.Save(sf.FileName);
+
+                    using(XmlWriter xw = XmlWriter.Create(sf.FileName))
+                    {
+                        xw.WriteStartDocument();
+                        xw.WriteStartElement("Kullanicilar");
+                        for (int i = 0; i < 3; i++)
+                        {
+                            xw.WriteStartElement("Kullanici");
+                            xw.WriteAttributeString("ID",i.ToString());
+                            xw.WriteElementString("Ad", textBox1.Text);
+                            xw.WriteElementString("Soyad", textBox2.Text);
+                            xw.WriteElementString("DTarih", textBox3.Text);
+                            xw.WriteElementString("Meslek", textBox4.Text);
+                            xw.WriteEndElement();
+                        }
+                        xw.WriteEndElement();
+                        xw.WriteEndDocument();
+                    }
+
+
+
+
+
+
+
+
+
+                    MessageBox.Show($"{sf.FileName} dosyası oluşturuldu.");
+                }
+            }
+        }
+
+        private void btnReadXML_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog of = new OpenFileDialog())
+            {
+                of.Filter = "XML Dosyaları|*.xml";
+                if (of.ShowDialog() == DialogResult.OK)
+                {
+                    XmlDocument xdoc = new XmlDocument();
+                    xdoc.Load(of.FileName);
+                    //XmlNodeList list = xdoc.SelectNodes("//Kullanicilar/Kullanici");
+                    //foreach (XmlNode x in list)
+                    //{
+                    //    textBox5.Text = x["Ad"].InnerText;
+                    //    textBox6.Text = x["Soyad"].InnerText;
+                    //    textBox7.Text = x["DTarih"].InnerText;
+                    //    textBox8.Text = x["Meslek"].InnerText;
+                    //}
+
+                    XmlNode x = xdoc.SelectSingleNode("//Kullanicilar/Kullanici");
+                    textBox5.Text = x["Ad"].InnerText;
+                    textBox6.Text = x["Soyad"].InnerText;
+                    textBox7.Text = x["DTarih"].InnerText;
+                    textBox8.Text = x["Meslek"].InnerText;
+                }
+            }
+        }
+
+       
+    }
+}
